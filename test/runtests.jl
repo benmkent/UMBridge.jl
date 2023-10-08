fa5ead1ece2e6f22a94766fdcdc5c651d7de18af
Marlena Weidenauer <134061260+marlenaweidenauer@users.noreply.github.com>
2023-10-08 15:04:22 +0200

    Update runtests.jl
    added three more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,3 +21,30 @@ end
     @test testmodel_supported_operations(model)
 
 end
+
+@testset "Test Connection" begin
+    
+    model=UMBridge.HTTPModel("posterior", "https://benchmark-analytic-funnel.linusseelinger.de")
+    httpValue = UMBridge.evaluate(model, [[1, 3]], Dict())[1][1]
+    exactValue = -5.147502395904501;
+    @test isapprox(httpValue, exactValue, rtol=1e-14)
+    
+end
+
+@testset "Test Apply Jacobian" begin
+    
+    model=UMBridge.HTTPModel("posterior", "https://benchmark-analytic-funnel.linusseelinger.de")
+    httpValue = UMBridge.apply_jacobian(model, 0, 0, [[1, 3]], [1, 4])[1]
+    exactValue = -3.370206919896928;
+    @test isapprox(httpValue, exactValue, rtol=1e-14)
+    
+end
+
+@testset "Test Gradient" begin
+    
+    model=UMBridge.HTTPModel("posterior", "https://benchmark-analytic-funnel.linusseelinger.de")
+    httpValue = UMBridge.gradient(model, 0, 0, [[1, 3]], [12])[1][1]
+    exactValue = 12.53215648992455;
+    @test isapprox(httpValue, exactValue, rtol=1e-14)
+    
+end
